99b29f0482ca59a6561d3524b1d38ef494424095
Rima24051950 <rimarz@yandex.ru>
2025-03-15 20:08:11 +0300

    Add files via upload

---
+++ Todo/Todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Sadyk on 15.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
